b458057a4dfc8f0e5816e29f3e17fe47bdb8795a
Brice Laroche <laroche.brice@gmail.com>
2022-05-09 20:07:00 +0200

    start frontend index

--- p7-frontend/src/components/header.js
+++ p7-frontend/src/components/header.js
@@ -5,7 +5,6 @@
 import React from "react"
 import logo from "../logos/icon-above-font.svg"
 
-
 export default function Header() {
     return (
         <header id="headerContainer">
--- p7-frontend/src/components/main.js
+++ p7-frontend/src/components/main.js
@@ -3,20 +3,32 @@
 */
 
 import React from "react"
+import logo from "../logos/icon.svg"
 
 export default function Main() {
     return (
         <main id="mainContent">
             <form id="mainContent__form">
-                <label>
-                    Email
-                </label>
-                <input type="email" className="inputForm"/>
-                <label>
-                    Password
-                </label>
-                <input type="password" className="inputForm"/>
+                <div className="form__container">
+                    <label className="emailLabel">
+                        Email     :      
+                    </label>
+                    <input type="email" className="inputForm"/>
+                </div>    
+                <div className="form__container">
+                    <label>
+                        Password :
+                    </label>
+                    <input type="password" className="inputForm"/>
+                </div>
+                <input type="submit" className="submitButton" value="Login" />
             </form>
+            <div className="img__container">
+                <img 
+                    src={logo}
+                    className="main--logo"
+                    alt="Groupomania World Logo"/>
+            </div>
         </main>
     )
 }
