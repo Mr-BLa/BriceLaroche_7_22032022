8e6e0d7ab5b3610fca8102dca12f6ae0fd7c2306
Brice Laroche <laroche.brice@gmail.com>
2022-05-10 10:31:13 +0200

    main.js: form Login

--- p7-frontend/src/components/main.js
+++ p7-frontend/src/components/main.js
@@ -6,6 +6,24 @@ import React from "react"
 import logo from "../logos/icon.svg"
 
 export default function Main() {
+    //Objet avec email et password
+    const [formLogin, setFormLogin] = React.useState(
+        {email: "", password: ""}
+    )
+    console.log(formLogin)
+
+    // On récupère notre objet avec tous ses composants et on actualise en fonction des éléments qui sont modifiés (via target.name)
+    function handleChange(event) {
+        setFormLogin(prevFormLogin => {
+            return {
+                ...prevFormLogin,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    
+
+
     return (
         <main id="mainContent">
             <form id="mainContent__form">
@@ -13,15 +31,28 @@ export default function Main() {
                     <label className="emailLabel">
                         Email     :      
                     </label>
-                    <input type="email" className="inputForm"/>
+                    <input 
+                        type="email" 
+                        className="inputForm" 
+                        onChange={handleChange}
+                        name="email"/>
+                        
                 </div>    
                 <div className="form__container">
                     <label>
                         Password :
                     </label>
-                    <input type="password" className="inputForm"/>
+                    <input 
+                        type="password" 
+                        className="inputForm" 
+                        onChange={handleChange}
+                        name="password"/>
+
                 </div>
-                <input type="submit" className="submitButton" value="Login" />
+                <input 
+                    type="submit" 
+                    className="submitButton" 
+                    value="Login" />
             </form>
             <div className="img__container">
                 <img 
