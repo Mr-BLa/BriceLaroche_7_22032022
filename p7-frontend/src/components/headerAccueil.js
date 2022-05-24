967ab34cc3720ec7bcb060c43a83b04c3df4db83
Brice Laroche <laroche.brice@gmail.com>
2022-05-24 10:01:16 +0200

    icon profillogo headerAccueil: chgt color

--- p7-frontend/src/components/headerAccueil.js
+++ p7-frontend/src/components/headerAccueil.js
@@ -17,10 +17,9 @@ export default function HeaderApp() {
                         src={HeaderAppLogo}
                         className="headerAccueil--logo"
                         alt="Groupomania Logo"/>
-                    <button
+                    <img
                         src={ProfilLogo}
-                        className="headerAccueil--profilLogo">
-                    </button>
+                        className="headerAccueil--profilLogo"/>
                 </nav>
             </header>
         </Link>
